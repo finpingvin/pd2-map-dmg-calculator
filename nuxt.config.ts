315691094d6e17f854e9f1aefb0acf892a856fd0
Björkander Daniel <pingvingryta@gmail.com>
2023-04-17 15:12:30 +0200

    For assets to load in github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/pd2-map-dmg-calculator/',
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
