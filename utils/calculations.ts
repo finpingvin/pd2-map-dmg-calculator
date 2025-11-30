ab3745e16644ced3fa2f0db0d8aadd58b4366b4e
Daniel Björkander <pingvingryta@gmail.com>
2025-11-30 07:53:24 +0100

    Fixed negative res resolution after Mayswan researched the logic for it.

--- utils/calculations.ts
+++ utils/calculations.ts
@@ -7,26 +7,31 @@ export enum ElementalDmgType {
   Poison,
 }
 
-export function calcRes(res: number, pierceNonBreaking: number = 0, pierceBreaking: number = 0): number {
+export function calcRes(
+  res: number,
+  pierceNonBreaking: number = 0,
+  pierceBreaking: number = 0,
+): number {
   let resAfterImmunityRemoval = res;
   if (res > 99) {
-    resAfterImmunityRemoval -= Math.floor(pierceBreaking / 2)
+    resAfterImmunityRemoval -= Math.floor(pierceBreaking / 2);
   } else {
-    resAfterImmunityRemoval -= pierceBreaking
+    resAfterImmunityRemoval -= pierceBreaking;
   }
 
   if (resAfterImmunityRemoval > 99) {
-    return resAfterImmunityRemoval
+    return resAfterImmunityRemoval;
   }
 
-  if ((resAfterImmunityRemoval - pierceNonBreaking) < 0) {
-    // Not sure if/how this is rounded within d2, but it is most often
-    // in a negative way for the player.
-    // should not make a big difference either way
-    return Math.floor((resAfterImmunityRemoval - pierceNonBreaking) / 2)
-  }
-
-  return resAfterImmunityRemoval - pierceNonBreaking
+  const resAfterPierceNonBreaking = resAfterImmunityRemoval - pierceNonBreaking;
+  // Not sure if/how this is rounded within d2, but it is most often
+  // in a negative way for the player.
+  // should not make a big difference either way
+  const resAfterAllPierce = resAfterPierceNonBreaking < 0
+    ? Math.ceil(resAfterPierceNonBreaking / 2)
+    : resAfterPierceNonBreaking;
+  // Negative pierce is capped to -100
+  return Math.max(resAfterAllPierce, -100);
 }
 
 export function calcDmg(
@@ -37,20 +42,21 @@ export function calcDmg(
   elementalDmgType: ElementalDmgType,
   considerPlr: boolean,
 ): number {
-  const resAfterPierce = calcRes(res, pierceNonBreaking, pierceBreaking)
+  const resAfterPierce = calcRes(res, pierceNonBreaking, pierceBreaking);
   if (resAfterPierce > 99) {
-    return 0
+    return 0;
   }
 
   let dmgAfterPierce;
   if (resAfterPierce < 0) {
-    dmgAfterPierce = dmg * (1 + (Math.abs(resAfterPierce) / 100))
+    dmgAfterPierce = dmg * (1 + (Math.abs(resAfterPierce) / 100));
   } else {
-    dmgAfterPierce = dmg * (1 - (resAfterPierce / 100))
+    dmgAfterPierce = dmg * (1 - (resAfterPierce / 100));
   }
 
   if (elementalDmgType === ElementalDmgType.Poison && considerPlr) {
-    return dmgAfterPierce * (1 + (Math.min(Math.abs(pierceNonBreaking / 2), 100) / 100))
+    return dmgAfterPierce *
+      (1 + (Math.min(Math.abs(pierceNonBreaking / 2), 100) / 100));
   }
-  return dmgAfterPierce
+  return dmgAfterPierce;
 }
